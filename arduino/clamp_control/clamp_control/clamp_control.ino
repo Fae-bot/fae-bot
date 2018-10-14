5b4563c29744aef1968614da5d09431cd530492b
Yves Quemener <yves@iv-devs.com>
2018-10-14 10:34:56 +0900

    Added clamp control

---
+++ arduino/clamp_control/clamp_control/clamp_control.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+Servo myservo;
+const int OPEN = 16;
+const int CLOSED = 60;
+
+int pos = 65;
+
+void setup() {
+  myservo.attach(9);
+  Serial.begin(57600);
+}
+
+void loop() {
+    myservo.write(pos);              
+    delay(150);                       
+  if (Serial.available() > 0) {
+    int value = Serial.parseInt();
+    pos = value;
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+    }
+    
+  }
+}
+
