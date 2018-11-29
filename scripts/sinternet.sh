74deac1e38d4a9f8cc9476fe66e7d5a655ce2d97
Yves Quemener <yves.quemener@gmail.com>
2018-11-29 12:18:04 +0900

    Typo in script

--- scripts/sinternet.sh
+++ scripts/sinternet.sh
@@ -1,5 +1,5 @@
 systemctl stop dnsmasq 
-wpa_supplicant_manual -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf -d
+wpa_supplicant_manual -iwlan0 -c/etc/wpa_supplicant/wpa_supplicant.conf -B
 sleep 5
 dhclient wlan0
 
