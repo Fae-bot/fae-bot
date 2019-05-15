c379a9069c5e7176e92576d94252b1970859a67e
Yves Quemener <yves@iv-devs.com>
2019-05-15 14:50:22 +0900

    Added a test to manage a subprocess and redirect its outputs as well as its display

---
+++ server/subprocess_test.py
@@ -0,0 +1,44 @@
+import subprocess as sb
+from pprint import pprint
+from time import sleep, clock
+from threading import Thread
+
+ARTK_PATH = "../../artoolkit5/bin/simpleLite"
+ARTK_ARGS = ['--vconf', '-dev=/dev/video0', '--thresh', '2']
+
+lines = list()
+process = None
+
+
+def artk_process():
+    global lines, process
+    process = sb.Popen([ARTK_PATH] + ARTK_ARGS,
+                       stdout=sb.PIPE,
+                       stderr=sb.PIPE,
+                       encoding="utf-8",
+                       env={'DISPLAY': ':0'})
+    while process.poll() is None:
+        #l = process.stdout.readline()
+        l = process.stdout.readline()
+        lines.append(l)
+        #print(l)
+        #print("added")
+        pass
+
+
+
+thr = Thread(target=artk_process)
+thr.start()
+
+starttime=clock()
+lastlen = 0
+try:
+    while len(lines)<1000:
+        if lastlen != len(lines):
+            pprint(lines[lastlen:len(lines)])
+            lastlen = len(lines)
+            #print(len(lines))
+
+except KeyboardInterrupt:
+    pass
+process.kill()
